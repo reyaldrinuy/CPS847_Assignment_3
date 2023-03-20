b4de6268f05d48ec6a74ec61e468cc91036fbac2
Damoon <damoon.shah3@gmail.com>
2023-03-20 16:56:59 -0400

    init

---
+++ src/components/TimeDisplay.vue
@@ -0,0 +1,34 @@
+<script>
+import { formatWithOptions } from 'date-fns/fp'
+import { eo } from 'date-fns/locale'
+export default {
+  data: () => ({
+    currentDate: null
+  }),
+  created() {
+    setInterval(this.getDateTime, 100);
+  },
+  methods: {
+    getDateTime() {
+      const d = formatWithOptions({ locale: eo },'yyyy-MM-dd\' \'HH:mm:ss')(new Date())
+      console.log(d)
+      this.currentDate = d
+      // this.currentDate = formatWithOptions(new Date(), 'yyyy-MM-dd\'T\'HH:mm:ssXX')
+    }
+  }
+}
+</script>
+
+<template>
+    <h3> {{ currentDate }}</h3>
+</template>
+
+<style scoped>
+h3 {
+  font-weight: 600;
+  color: white;
+  font-size: 1.2rem;
+  top: 10px;
+  text-align: left;
+}
+</style>
