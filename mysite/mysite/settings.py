7b6c9cbe0bed4d09d729fab8c552e1ff07ba4b88
Damoon <damoon.shah3@gmail.com>
2023-03-28 14:34:22 -0400

    added the facilitor to interface

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&(ey6ttsrc6qb^*cq)*t5d-oc33cgfjnk)gzc1(3g2h00x)nt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
