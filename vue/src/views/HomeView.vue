b3e5c1b9258c7eb66f54af5ddf892cbf759be569
Damoon <damoon.shah3@gmail.com>
2023-03-28 14:33:51 -0400

    removed boilerplate code

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>We can help with your future</h1>
+    <h3>Contact us now!</h3>
   </main>
 </template>
