c4956a0277cd9949405fad92e6ed084c21b93e5c
Damoon <damoon.shah3@gmail.com>
2023-03-30 21:06:27 -0400

    Added the IP to allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-&(ey6ttsrc6qb^*cq)*t5d-oc33cgfjnk)gzc1(3g2h00x)nt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
-
+ALLOWED_HOSTS = ['192.168.60.123', '0.0.0.0']
 
 # Application definition
 
